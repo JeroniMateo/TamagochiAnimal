63b43b8e5a3bab700bb2546baadc2ada9c2da946
Maite <mladaria@cifpfbmoll.eu>
2021-02-15 01:04:09 +0100

    He añadido los atributos. Falta añadir: resetLifeBeing(…): reiniciará el lifeBeing. No modifica las acciones realizadas ya que es una nueva accion ◦ render: devuelve el renderizado del LifeBeing como creas necesario ◦ getStatus: devuelve el status del LifeBeing

--- src/main/java/app/service/ServiceGame.java
+++ src/main/java/app/service/ServiceGame.java
@@ -1,8 +1,19 @@
 package app.service;
 
+import app.p0.data.Tamagochi;
+import app.p0.data.UserAction;
 import org.springframework.stereotype.Service;
+
+import javax.swing.*;
+import java.util.List;
 
 @Service
 public class ServiceGame {
+private List<UserAction> userActions;
+private Integer MaxPoints;
+Tamagochi currentLifeBeing=new Tamagochi();
+
+public void doAction(Action action){}
+public void reseLifeBeing(){}
 
 }
